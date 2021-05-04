f29ef9ebbeb1c250351b19a6613c7f148a6f8d07
thomas <mryoungtommy@gmail.com>
2021-05-04 06:12:43 +0300

    dropdown menu

---
+++ src/app/_services/header.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/internal/Subject';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  private dataSource = new Subject();
+  page: string;
+
+  constructor() { }
+
+  getDataObservable() {
+    return this.dataSource;
+  }
+
+  setPage(page) {
+    this.page = page;
+    this.sendData();
+  }
+
+  getPage() {
+    return this.page;
+  }
+
+  sendData() {
+    console.log('sendData');
+    this.dataSource.next({
+      page: this.page,
+    });
+  }
+  
+}
--- src/app/user/shared/header/header.component.ts
+++ src/app/user/shared/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/_services/authentication.service';
+import { HeaderService } from 'src/app/_services/header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  user: any = {};
+  headerDataObserver: any;
+  page: string;
+
+  constructor(
+    private auth: AuthenticationService,
+    private headerService: HeaderService,
+    private router: Router
+  ) {
+    this.page = headerService.getPage();
+    
+    this.headerDataObserver = this.headerService.getDataObservable();
+    this.headerDataObserver.subscribe((data: any) => {
+        console.log('observarable', data);
+        this.page = data.page;
+      }
+    );
+  }
 
   ngOnInit(): void {
+    this.auth.isLoggedIn()
+    .then((data: any) => {
+      this.user = data;
+    })
+    .catch(err => {
+      console.log('Error getting user.');
+    });
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/auth']);
   }
 
 }
