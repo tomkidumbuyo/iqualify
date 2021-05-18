14323d336f72b6f3a5f0efe332c884beaa222c98
thomas <mryoungtommy@gmail.com>
2021-05-18 11:51:50 +0300

    home page

--- src/app/leaner/leaner-routing.module.ts
+++ src/app/leaner/leaner-routing.module.ts
@@ -1,13 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BuyComponent } from './buy/buy.component';
-import { CertificatesComponent } from './certificates/certificates.component';
+import { CertificatesComponent } from './dashboard/certificates/certificates.component';
 import { CourseComponent } from './course/course.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LeanerComponent } from './leaner.component';
 import { LessonComponent } from './lesson/lesson.component';
-import { MyCoursesComponent } from './my-courses/my-courses.component';
+import { MyCoursesComponent } from './dashboard/my-courses/my-courses.component';
 import { TopicComponent } from './topic/topic.component';
+import { HomeComponent } from './dashboard/home/home.component';
 
 const routes: Routes = [{
   path: 'leaner',
@@ -20,6 +21,9 @@ const routes: Routes = [{
     path: 'home',
     component: DashboardComponent,
     children: [{
+      path: '',
+      component: HomeComponent
+    }, {
       path: 'mycourses',
       component: MyCoursesComponent
     }, {
--- src/app/leaner/leaner.module.ts
+++ src/app/leaner/leaner.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeanerComponent } from './leaner.component';
-import { CertificatesComponent } from './certificates/certificates.component';
+import { CertificatesComponent } from './dashboard/certificates/certificates.component';
 import { CourseComponent } from './course/course.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LessonComponent } from './lesson/lesson.component';
-import { MyCoursesComponent } from './my-courses/my-courses.component';
+import { MyCoursesComponent } from './dashboard/my-courses/my-courses.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ScoreIllustrationComponent } from './shared/score-illustration/score-illustration.component';
@@ -13,7 +13,6 @@ import { TopicComponent } from './topic/topic.component';
 import { LeanerRoutingModule } from './leaner-routing.module'
 import { SharedModule } from '../shared/shared.module';
 import { BuyComponent } from './buy/buy.component';
-import { HomeComponent } from './home/home.component';
 
 
 
@@ -28,7 +27,8 @@ import { HomeComponent } from './home/home.component';
     CourseComponent, 
     TopicComponent, 
     LessonComponent, 
-    ScoreIllustrationComponent, BuyComponent, HomeComponent
+    ScoreIllustrationComponent, 
+    BuyComponent,
   ],
   imports: [
     CommonModule,
